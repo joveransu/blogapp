1bd4c57979442be0dc4ea4d1e61c2e2faeb84133
Anderson Alencar <joveransu@gmail.com>
2022-10-14 21:08:00 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 //rodando em ambiente e3xterno
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: "mongodb+srv://ansu:2377@cluster0.16vdayl.mongodb.net/?retryWrites=true&w=majority"}
-//                               mongodb+srv://ansu:2377@cluster0.16vdayl.mongodb.net/<DATABASE> 
+    module.exports = {mongoURI: "mongodb+srv://ansu:2377@cluster0.16vdayl.mongodb.net/blogapp"}
+//                               mongodb+srv://ansu:2377@cluster0.16vdayl.mongodb.net/?retryWrites=true&w=majority"
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
